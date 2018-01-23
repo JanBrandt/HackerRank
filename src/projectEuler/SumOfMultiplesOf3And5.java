3af3df651161da8d5b48336bc101258a5cf8ea14
Jan Brandt <jbrandt1@freenet.de>
2018-01-23 17:30:36 +0100

    Started with projectEuler, first files for Problem 1

---
+++ src/projectEuler/SumOfMultiplesOf3And5.java
@@ -0,0 +1,17 @@
+/**
+ *
+ */
+package projectEuler;
+
+/**
+ * @author Jan Brandt
+ * @version 0.01 23/01/2018
+ *
+ */
+public class SumOfMultiplesOf3And5 {
+  
+  public int sumOfMultiplesOf3And5(int zahl) {
+    return 0;
+  }
+
+}
---
+++ src/projectEuler/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Collection of Classes for solving the problems of the ProjectEuler
+ * 
+ * @author Jan Brandt
+ * @version 1
+ * 
+ */
+package projectEuler;
