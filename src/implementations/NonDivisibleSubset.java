72adf9fe8c2e598d83037ed4aa854e96b6349b87
Jan Brandt <jbrandt1@freenet.de>
2018-01-24 11:55:23 +0100

    Solved implementations problem NonDivisibleSubset

--- src/implementations/NonDivisibleSubset.java
+++ src/implementations/NonDivisibleSubset.java
@@ -2,13 +2,27 @@ package implementations;
 
 /**
  * @author Jan Brandt
- * @version 0.01 24/01/2018
+ * @version 0.1 24/01/2018
  *
  */
 public class NonDivisibleSubset {
   
-  public int nonDivisibleSubset(int k, int[] arr) {
-    return 0;
+  public static int nonDivisibleSubset(int k, int[] arr) {
+    int result = 0;
+    int[] remainders = new int[k];
+    for (int i = 0; i < arr.length; i++) {
+      remainders[arr[i]%k]++;
+    }
+    if (remainders[0] > 0) result++; //Max one item with remainder zero
+    for (int i = 1; i <= remainders.length/2; i++) {
+      if ((2*i)%k == 0) { //If remainders is k/2, also only one item may be added
+        if (remainders[i]> 0) result++;
+      } else {
+        int max = Math.max(remainders[i], remainders[k-i]);
+        result += max;
+      }
+    }
+    return result;
   }
 
 }
--- src/implementations/tests/NonDivisibleSubsetTest.java
+++ src/implementations/tests/NonDivisibleSubsetTest.java
@@ -10,11 +10,26 @@ public class NonDivisibleSubsetTest {
 
   @Test
   public void testNonDivisibleSubsetSample() {
-    NonDivisibleSubset nds = new NonDivisibleSubset();
     int[] array= {1,7,2,4};
     int k = 3;
-    int result = nds.nonDivisibleSubset(k, array);
+    int result = NonDivisibleSubset.nonDivisibleSubset(k, array);
     assertEquals("Sample 1", 3, result);
+  }
+  
+  @Test
+  public void testNonDivisibleSubsetSimple() {
+    int[] array = {3};
+    int k = 3;
+    int result = NonDivisibleSubset.nonDivisibleSubset(k, array);
+    assertEquals("Simple tests 1", 1, result);
+    int[] array2 = {3,6,7,8};
+    k = 3;
+    result = NonDivisibleSubset.nonDivisibleSubset(k, array2);
+    assertEquals("Simple tests 1", 2, result);
+    int[] array3= {0,1,2,3,4,5,6,7,8,9,10};
+    k = 10;
+    result = NonDivisibleSubset.nonDivisibleSubset(k, array3);
+    assertEquals("Simple tests 1", 6, result);
   }
 
 }
