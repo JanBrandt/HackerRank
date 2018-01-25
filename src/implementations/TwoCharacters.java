342a0ff8f285d453868fb2d472ade927849af8ad
Jan Brandt <jbrandt1@freenet.de>
2018-01-25 10:54:51 +0100

    Started new problem TwoCharacters in Implementations

---
+++ src/implementations/TwoCharacters.java
@@ -0,0 +1,14 @@
+package implementations;
+
+/**
+ * @author Jan Brandt
+ * @version 0.01 25/01/2018
+ *
+ */
+public class TwoCharacters {
+  
+  public static int twoCharacters(String s) {
+    return 0;
+  }
+
+}
---
+++ src/implementations/tests/TwoCharactersTest.java
@@ -0,0 +1,14 @@
+package implementations.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TwoCharactersTest {
+
+  @Test
+  public void testTwoCharacters() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
